3f3994767e99753855ce6695bc5962519091524e
SoniaGrh <sonia.garrouch@hotmail.fr>
2025-04-25 14:40:12 +0000

    feat: add smoke test

--- picsellia_cli/commands/training/__init__.py
+++ picsellia_cli/commands/training/__init__.py
@@ -3,9 +3,11 @@ import typer
 from .deployer import deploy_pipeline
 from .initializer import init_training_pipeline
 from .tester import test
+from .smoke_tester import smoke_test
 
 app = typer.Typer(help="Manage training pipelines.")
 
 app.command("init")(init_training_pipeline)
 app.command("test")(test)
 app.command("deploy")(deploy_pipeline)
+app.command("smoke-test")(smoke_test)
