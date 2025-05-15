bfb6693ceb6e306f2b2e84060dae787903ab5c44
SoniaGrh <sonia.garrouch@hotmail.fr>
2025-05-15 13:54:44 +0200

    fix: handle dashes

--- picsellia_cli/utils/initializer.py
+++ picsellia_cli/utils/initializer.py
@@ -3,6 +3,7 @@ import os
 from picsellia import Client
 from picsellia.exceptions import ResourceNotFoundError
 from picsellia.types.enums import Framework, InferenceType
+import typer
 
 
 def write_pipeline_file(path: str, content: str):
@@ -37,3 +38,35 @@ def create_or_get_model_version(
                 "image_size": 640,
             },
         )
+
+
+def handle_pipeline_name(pipeline_name: str) -> str:
+    """
+    This function checks if the pipeline name contains dashes ('-') and prompts the user to either
+    replace them with underscores ('_') or modify the name entirely.
+
+    Args:
+        pipeline_name (str): The original pipeline name to check and modify.
+
+    Returns:
+        str: The modified pipeline name.
+    """
+    if "-" in pipeline_name:
+        replace_dashes = typer.prompt(
+            f"The pipeline name '{pipeline_name}' contains a dash ('-'). "
+            "Would you like to replace all dashes with underscores? (yes/no)",
+            type=str,
+            default="yes",
+        ).lower()
+
+        if replace_dashes == "yes":
+            pipeline_name = pipeline_name.replace("-", "_")
+            typer.echo(f"✅ The pipeline name has been updated to: '{pipeline_name}'")
+        else:
+            pipeline_name = typer.prompt(
+                "Please enter a new pipeline name without dashes ('-'):",
+                type=str,
+            )
+            typer.echo(f"✅ The pipeline name has been updated to: '{pipeline_name}'")
+
+    return pipeline_name
