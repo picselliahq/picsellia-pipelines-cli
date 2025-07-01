1a1ed499cc3f4470a7fc1ffcc7876c5ec3ef150f
SoniaGrh <sonia.garrouch@hotmail.fr>
2025-07-01 17:46:24 +0200

    fix: param class name

--- picsellia_cli/commands/training/templates/ultralytics_template.py
+++ picsellia_cli/commands/training/templates/ultralytics_template.py
@@ -120,7 +120,7 @@ TRAINING_PIPELINE_PARAMETERS = """from picsellia.types.schemas import LogDataTyp
 from picsellia_cv_engine.core.parameters import HyperParameters
 
 
-class SimpleHyperParameters(HyperParameters):
+class TrainingHyperParameters(HyperParameters):
     def __init__(self, log_data: LogDataType):
         super().__init__(log_data=log_data)
         self.epochs = self.extract_parameter(["epochs"], expected_type=int, default=3)
